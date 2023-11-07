172198e9540d4c04c43fd9c9fda8846c8f774567
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2023-11-07 19:16:03 +0530

    added 6 pictures added code in compare_trac.php all ui is done except the one tab comparing section

--- compare_trac.php
+++ compare_trac.php
@@ -24,147 +24,279 @@
     <!-- All News Section -->
     <div class="container" id="an">       
         <div class="row py-1">
-         <h1 class="mt-2 mb-3">All News</h1> 
-            <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                        <a href="#">
-                          <div class="">
-                          <img src="assets\images\sarkari-yojana-1698841100.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content mb-3 pb-3 ms-3">
-                        <a href="#" class="text-decoration-none text-dark ">
-                          <h4 class="fw-bold mt-1">Same Deutz Fahr Agromaxx 40 </h3>
-                        </a>
-                        <div class="row mt-1 ps-3">
-                            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fas fa-bolt"></i> 50HP
-                            </p>
-                        </div>
-                        <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fa fa-cog" aria-hidden="true"></i> 3000 CC
-                            </p>
-                        </div>
-                        </div>
-                        <a href="#" class="text-decoration-none text-dark pb-1">
-                          <span class=""> Get On Road price </span>
-                          <span class="icon">
-                            <i class="fa-solid fa-chevron-right"></i>
-                          </span>
-                        </a>
-                    </div>
-                </div>
-            </div>                
-            <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                        <a href="#">
-                          <div class="">
-                          <img src="assets\images\sarkari-yojana-1698920602.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content ms-3">
-                        <a href="#" class="text-decoration-none text-dark ">
-                          <h4 class="fw-bold mt-3">Same Deutz Fahr Agromaxx 40 </h3>
-                        </a>
-                        <div class="row mt-1 ps-3">
-                            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fas fa-bolt"></i> 50HP
-                            </p>
-                        </div>
-                        <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fa fa-cog" aria-hidden="true"></i> 3000 CC
-                            </p>
-                        </div>
-                    </div>
-                        <a href="#" class="text-decoration-none text-dark mb-4">
-                          <span> Get On Road price </span>
-                          <span class="icon">
-                            <i class="fa-solid fa-chevron-right"></i>
-                          </span>
-                        </a>
-                    </div>
-                </div>
-            </div>                
+         <h3 class="mt-2 mb-3 bg-light">Compare Tractors</h3>
 
             <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                        <a href="#">
-                          <div class="">
-                          <img src="assets/images/sarkari-yojana-1698923114.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content ms-3">
-                        <a href="#" class="text-decoration-none text-dark ">
-                          <h6 class="mt-3">स्क्वायर बेलर मशीन पर मिल रही है 6,25,000 रुपए की...</h6>
-                        </a>
-                        <div class="row mt-1 ps-3">
-                            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fas fa-bolt"></i> 50HP
-                            </p>
+               <div class="success__stry__item shadow h-100">
+                   <div class="thumb">
+                      <a href="#">
+                        <div class="">
+                         <img src="assets\images\default-image.jpg" class="object-fit-cover p-3 w-100" alt="img">
                         </div>
-                        <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fa fa-cog" aria-hidden="true"></i> 3000 CC
-                            </p>
-                        </div>
+                      </a>
+                   </div>
+                   <div class="content mt-5 py-5 pb-3 text-center">                        
+                      <a href="#" class="text-decoration-none fs-5 text-primary pb-1">
+                        <span class="">Select Tractor </span>                          
+                      </a>                     
+                   </div>
+                   <div class="col-12  text-center"> 
+                      <button class="btn btn-success text-white col-12 px-5">Compare</button>
                     </div>
-                        <a href="#" class="text-decoration-none text-dark mb-4">
-                          <span> Get On Road price </span>
-                          <span class="icon">
-                            <i class="fa-solid fa-chevron-right"></i>
-                          </span>
-                        </a>
-                    </div>
-                </div>
-            </div>    
-            
-            <div class="col-12 col-lg-3 col-sm-3 col-md-3 pt-3  mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                        <a href="#">
-                          <div class="">
-                          <img src="assets\images\tractor-1698836924.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content ms-3 mb-4">
-                        <a href="#" class="text-decoration-none text-dark ">
-                          <h4 class="fw-bold mt-3">Same Deutz Fahr Agromaxx 40 </h3>
-                        </a>
-                        <div class="row mt-1 ps-3">
-                            <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fas fa-bolt"></i> 50HP
-                            </p>
-                        </div>
-                        <div class="col-12 col-lg-6 col-md-6 col-sm-6">
-                            <p class="">
-                              <i class="fa fa-cog" aria-hidden="true"></i> 3000 CC
-                            </p>
-                        </div>
-                    </div>
-                        <a href="#" class="text-decoration-none text-dark mb-4">
-                          <span> Get On Road price </span>
-                          <span class="icon">
-                            <i class="fa-solid fa-chevron-right"></i>
-                          </span>
-                        </a>
-                    </div>
-                </div>
+               </div>
             </div>   
+
+            <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+              <div class="success__stry__item shadow h-100">
+                <div class="thumb">
+                  <a href="#">
+                    <div class="">
+                      <img src="assets\images\default-image.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                    </div>
+                  </a>
+                </div>
+                <div class="content mt-5 py-5 pb-3 text-center">                        
+                  <a href="#" class="text-decoration-none fs-5 text-primary pb-1">
+                    <span class="">Select Tractor </span>                          
+                  </a>
+                </div>
+                <div class="col-12  text-center"> 
+                  <button class="btn btn-success text-white col-12 px-5">Compare</button>
+                </div>                
+              </div>
+            </div>   
+
+            <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+              <div class="success__stry__item shadow h-100">
+                <div class="thumb">
+                  <a href="#">
+                    <div class="">
+                      <img src="assets\images\default-image.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                    </div>
+                  </a>
+                </div>
+                <div class="content mt-5 py-5 pb-3 text-center">                        
+                  <a href="#" class="text-decoration-none fs-5 text-primary pb-1">
+                    <span class="">Select Tractor </span>                          
+                  </a>
+                </div>
+                <div class="col-12  text-center"> 
+                  <button class="btn btn-success text-white col-12 px-5">Compare</button>
+                </div>  
+              </div>
+            </div>               
+            
+            <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+              <div class="success__stry__item shadow h-100">
+                <div class="thumb">
+                  <a href="#">
+                    <div class="">
+                      <img src="assets\images\default-image.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                    </div>
+                  </a>
+                </div>
+                <div class="content mt-5 py-5 pb-3 text-center">                        
+                  <a href="#" class="text-decoration-none fs-5 text-primary pb-1">
+                    <span class="">Select Tractor </span>                          
+                  </a>
+                </div>
+                <div class="col-12  text-center"> 
+                  <button class="btn btn-success text-white col-12 px-5">Compare</button>
+                </div>  
+              </div>
+            </div>   
+
         </div>
     </div>
+  <section>
+    <div class="container">
+      <div class=" ps-5 pe-5">
+        <a href="#" class="text-decoration-none fs-5 text-primary pb-1">
+          <img src="assets\images\5-year-warranty-Mobile-Banner_1077x168Pxl.avif" class="object-fit-cover p-3 w-100" alt="img">                       
+        </a>      
+      </div>
+    </div>
+  </section>
+
+  <!-- Compare To Buy The Right Tractor  -->   <!--WITH THE "TABS"-->
+  <section>
+    <div class="container bg-light">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Under 20 HP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">21-30</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">31-40</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">41-45</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">46-50</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Above 50</a>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- CONTENT IN THE TABS -->
+  <section>
+    <div class="container">
+    <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+            <div class="success__stry__item shadow h-100">
+              <div class="thumb">
+                <a href="#">
+                  <div class="">
+                    <img src="assets\images\mqdefault 1.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                  </div>
+                </a>
+              </div>
+              <div class="content mb-3 pb-3 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                  <h4 class="fs-6 mt-1">Compare Tractors 5060e and 6010...</h3>
+                </a>
+                <a href="#" class="text-decoration-none fs-6 text-dark">
+                  <span class="">-23 jun 2020 </span>                  
+                </a>
+              </div>
+            </div>
+          </div> 
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+  <section>
+    <div class="container">       
+      <div class="row py-1 bg-lighta">
+        <h1 class="mt-2 mb-3 fs-5 mt-4">Watch Tractors Comparison Videos</h1> 
+          <!-- 1st comparision card -->
+          <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+            <div class="success__stry__item shadow h-100">
+              <div class="thumb">
+                <a href="#">
+                  <div class="">
+                    <img src="assets\images\mqdefault 1.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                  </div>
+                </a>
+              </div>
+              <div class="content mb-3 pb-3 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                  <h4 class="fs-6 mt-1">Compare Tractors 5060e and 6010...</h3>
+                </a>
+                <a href="#" class="text-decoration-none fs-6 text-dark">
+                  <span class="">-23 jun 2020 </span>                  
+                </a>
+              </div>
+            </div>
+          </div> 
+          <!-- 2nd comparision card -->
+          <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+            <div class="success__stry__item shadow h-100">
+              <div class="thumb">
+                <a href="#">
+                  <div class="">
+                    <img src="assets\images\mqdefault 2.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                  </div>
+                </a>
+              </div>
+              <div class="content mb-3 pb-3 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                  <h4 class="fs-6 mt-1">Massey Ferguson 7250 Power vs M...</h3>
+                </a>
+                <a href="#" class="text-decoration-none fs-6 text-dark">
+                  <span class="">-09 jul 2020 </span>                  
+                </a>
+              </div>
+            </div>
+          </div> 
+          <!-- 3rd comparision card -->
+          <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+            <div class="success__stry__item shadow h-100">
+              <div class="thumb">
+                <a href="#">
+                  <div class="">
+                    <img src="assets\images\mqdefault 3.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                  </div>
+                </a>
+              </div>
+              <div class="content mb-3 pb-3 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                  <h4 class="fs-6 mt-1">हरियाणा में हैरो मुकाबला : इस ट्रैक्टर ने...</h3>
+                </a>
+                <a href="#" class="text-decoration-none fs-6 text-dark">
+                  <span class="">-28 Nov 2020 </span>                  
+                </a>
+              </div>
+            </div>
+          </div> 
+          <!-- 4th comparision card -->
+          <div class="col-12 col-lg-3 col-sm-3 col-md-3 mt-2 mb-2">
+            <div class="success__stry__item shadow h-100">
+              <div class="thumb">
+                <a href="#">
+                  <div class="">
+                    <img src="assets\images\mqdefault 4.jpg" class="object-fit-cover p-3 w-100" alt="img">
+                  </div>
+                </a>
+              </div>
+              <div class="content mb-3 pb-3 ms-3">
+                <a href="#" class="text-decoration-none text-dark ">
+                  <h4 class="fs-6 mt-1">Agriculture News , सरकारी योजनाएं , T...</h3>
+                </a>
+                <a href="#" class="text-decoration-none fs-6 text-dark">
+                  <span class="">-23 jun 2020 </span>                  
+                </a>
+              </div>
+            </div>
+          </div> 
+
+      </div>
+    </div>    
+  </section>
+
+
+  <section>
+    <div class="container">
+      <div class=" fw-bold fs-5 mt-3 ">
+        <p class="mb-n4">Compare Tractors</p>
+      </div>
+      <p class="">Tractorjunction.com is a one-stop authentic online destination where you can compare a variety of Tractors and Farm Implements. All top tractor brands are available here including Mahindra, John Deere, Escorts, Sonalika, Eicher, TAFE, New Holland and many more. The information displayed on Tractor Junction is believed to be accurate, unbiased and correct. Choose at least two tractors as per your choice to compare based on their specifications, features, mileage, Price, overall perf<span id="dots">...</span><span id="more">ormance and warranty. All Indian Farmers can easily compare tractors of distinct varieties just in a few clicks. TractorJunction brings a welfare opportunity to compare tractor price in India. This allows farmers from every region to compare tractors in India.<br><br>
+
+      TractorJunction provides the most comprehensive tractor comparison tool in India on which you can select at least two or more tractors of your choice for comparison. This online platform provides all the useful guidelines for tractor comparison India. TractorJunction always works to empower Indian farmers with a new tractor compare section.<br><br>
+
+      Compare tractor prices in India, specifications, warranty and many more at one place and then select your dream tractor. For Further more inquiries stay tuned with TractorJunction.</span></p>
+      <button class="text-primary" onclick="myFunction()" id="myBtn">Read more</button>
+      <script>
+        function myFunction() {
+          var dots = document.getElementById("dots");
+          var moreText = document.getElementById("more");
+          var btnText = document.getElementById("myBtn");
+
+          if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more"; 
+            moreText.style.display = "none";
+          } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less"; 
+            moreText.style.display = "inline";
+          }
+        }
+      </script>
+    </div>
+  </section>
 
    <?php
    include 'includes/footer.php';
