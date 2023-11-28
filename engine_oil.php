2203bff34a8ed89460202292c5a0cbbebe0423a9
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2023-11-28 12:02:43 +0530

    update code

--- engine_oil.php
+++ engine_oil.php
@@ -20,143 +20,32 @@
     </section>
 
     <section>
-    <div class="container" id="an">       
-      <div class="row py-1">
-        <h1 class="mt-2 mb-3">Engine Oil</h1> 
-
-            <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                        <a href="news_content.php">
-                          <div class="">
-                            <img src="assets\images\sarkari-yojana-1698841100.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content mb-3 ms-3">
-                        <button type="button" class="btn btn-warning">Latest News</button>
-                        <div class="row mt-2">
-                            <p>खुशखबरी दिवाली पर 1.75 करोड़ परिवारों को फ्री में गैस...</p>
-                        </div>
-                        <a href="#" class="text-decoration-none text-dark pb-1">
-                          <span class=""> 23-November-2023 </span>
-                        </a>
-                    </div>
-                </div>
-            </div>   
-
-            <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                      <a href="news_content.php">
-                        <div class="">
-                          <img src="assets\images\sarkari-yojana-1698920602.webp" class="object-fit-cover p-3 w-100" alt="img">
-                        </div>
-                      </a>
-                    </div>
-                    <div class="content ms-3">
-                      <button type="button" class="btn btn-warning">Sarkari Yojana News</button>
-                      <div class="row mt-1 ps-0">
-                        <p class="mt-2">लाड़ली बहना योजना : महिलाओं को 7वीं किस्त में मिल सक...</p>
-                      </div>
-                      <a href="#" class="text-decoration-none text-dark">
-                        <span> 23-November-2023 </span>
-                      </a>
-                    </div>
-                </div>
-            </div>                
-
+      <div class="container" id="an">       
+        <div class="row py-1">
+          <h1 class="mt-2 mb-3">Engine Oil</h1> 
             <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
               <div class="success__stry__item shadow h-100">
                 <div class="thumb">
-                  <a href="news_content.php">
+                  <a href="">
                     <div class="">
-                      <img src="assets/images/sarkari-yojana-1698923114.webp" class="object-fit-cover p-3 w-100" alt="img">
+                      <img src="assets\images\engineoiltotal.webp" class="object-fit-cover engineoil_img p-3 w-100" alt="img">
                     </div>
                   </a>
                 </div>
-                <div class="content ms-3">
-                  <button type="button" class="btn btn-warning">Sarkari Yojana News</button>
-                  <div class="row mt-1 ps-0">
-                    <p class="mt-2">स्क्वायर बेलर मशीन पर मिल रही है 6,25,000 रुपए की...</p>
-                  </div>
-                  <a href="#" class="text-decoration-none text-dark">
-                    <span> 23-November-2023 </span>
-                  </a>
+              <div class="content mb-3 ms-3">
+                <button type="button" class="btn btn-primary">Mahindra Engine Oil</button>
+                <div class="row mt-2">
+                  <p>खुशखबरी दिवाली पर 1.75 करोड़ परिवारों को फ्री में गैस...</p>
                 </div>
-              </div>
-            </div>            
-            
-            <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                      <a href="news_content.php">
-                        <div class="">
-                          <img src="assets\images\tractor-1698836924.webp" class="object-fit-cover p-3 w-100" alt="img">
-                        </div>
-                      </a>
-                    </div>
-                    <div class="content mb-3 ms-3">
-                        <button type="button" class="btn btn-warning">Latest News</button>
-                        <div class="row mt-2">
-                            <p>खुशखबरी दिवाली पर 1.75 करोड़ परिवारों को फ्री में गैस...</p>
-                        </div>
-                        <a href="#" class="text-decoration-none text-dark pb-1">
-                          <span class=""> 23-November-2023 </span>
-                        </a>
-                    </div>
-                </div>
-            </div>   
-
-            <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
-                <div class="success__stry__item shadow h-100">
-                    <div class="thumb">
-                      <a href="news_content.php">
-                          <div class="">
-                          <img src="assets\images\tractor-1698836924.webp" class="object-fit-cover p-3 w-100" alt="img">
-                          </div>
-                        </a>
-                    </div>
-                    <div class="content ms-3">
-                      <button type="button" class="btn btn-warning">Sarkari Yojana News</button>
-                      <div class="row mt-1 ps-0">
-                        <p class="mt-2">लाड़ली बहना योजना : महिलाओं को 7वीं किस्त में मिल सक...</p>
-                      </div>
-                      <a href="#" class="text-decoration-none text-dark">
-                        <span> 23-November-2023 </span>
-                      </a>
-                    </div>
-                </div>
-            </div>                
-
-            <div class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2">
-              <div class="success__stry__item shadow h-100">
-                <div class="thumb">
-                  <a href="news_content.php">
-                    <div class="">
-                    <img src="assets/images/250-di-1630740119.webp" class="object-fit-cover p-3 w-100" alt="img">
-                    </div>
-                  </a>
-                </div>
-                <div class="content ms-3">
-                  <button type="button" class="btn btn-warning">Sarkari Yojana News</button>
-                  <div class="row mt-1 ps-0">
-                    <p class="mt-2">स्क्वायर बेलर मशीन पर मिल रही है 6,25,000 रुपए की...</p>
-                  </div>
-                  <a href="#" class="text-decoration-none text-dark">
-                    <span> 23-November-2023 </span>
-                  </a>
-                </div>
+                <a href="#" class="text-decoration-none text-dark pb-1">
+                  <span class=""> 23-November-2023 </span>
+                </a>
               </div>
             </div>
+          </div>   
+        </div>
       </div>
-    </div>
     </section>
-
-
-
-
-
     <?php
        
         include 'includes/footer.php';
