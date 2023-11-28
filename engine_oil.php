8f2234e7cb5f03f930a7eb9d05befe931b022f8e
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2023-11-28 16:53:03 +0530

    updated code

--- engine_oil.php
+++ engine_oil.php
@@ -909,6 +909,65 @@
             </div>
           </a>
           
+          <a href="engine_oil_inner.php" class="col-12 col-lg-4 col-sm-4 col-md-4 mt-2 mb-2 text-decoration-none">           
+            <div class="success__stry__item h-100 shadow text-dark">
+              <div class="thumb">
+                <div href="">
+                  <div class="">
+                    <img src="assets\images\engineoiltotal.webp" class="object-fit-cover engineoil_img p-3 w-100" alt="img">
+                  </div>
+                </div>
+              </div>
+              <div class="content mb-0 ms-3">
+                <p class="fs-5 fw-bold" >Mahindra </p>
+                <p class="text-success text-center me-3 fw-bold border border-success border-2 rounded-pill hover">M-Star Mahindra Genuine Engine Oil</p>
+                <div class="row mt-1">
+                  <div class="col-12 col-lg-6 col-md-6 col-sm-6">
+                    <div class="row">
+                      <div class="col-12 col-lg-4 col-md-4 col-sm-4">
+                        <p>Grade- </p>
+                      </div> 
+                      <div class="col-12 col-lg-8  col-md-8 col-sm-8">
+                        <p> 20W</p>
+                      </div>  
+                    </div>                                                     
+                  </div>
+
+                  <div class="col-12 col-lg-6 col-md-6 col-sm-6">
+                    <div class="row">
+                      <div class="col-12 col-lg-4 col-md-4 col-sm-4">
+                        <p>Quantity-</p>
+                      </div>  
+                      <div class="col-12 col-lg-8 col-md-8 col-sm-8">
+                        <p>7L</p>
+                      </div>  
+                    </div>
+                  </div>                  
+                </div>
+                <div class="row">
+                  <div class="col-12 col-lg-2 col-md-2 col-sm-2">
+                    <p>price-</p>
+                  </div>
+                  <div class="col-12 col-lg-10 col-md-10 col-sm-10 text-start">
+                    <p>4099</p>
+                  </div>                  
+                </div>
+                <div class="row">
+                  <div class="col-12 col-lg-5 col-md-5 col-sm-5">
+                    <p>Compatible Tractor</p>
+                  </div>
+                  <div class="col-12 col-lg-3 col-md-3 col-sm-3 text-start">
+                    <p>DI 400...</p>
+                  </div>                 
+                  <div class="col-12 col-lg-4 col-md-4 col-sm-4 text-start text-primary">
+                    <p>View More</p>
+                  </div>                 
+                  
+                </div>  
+              </div>
+            </div>
+          </a>
+
         </div>
       </div>
     </section>
