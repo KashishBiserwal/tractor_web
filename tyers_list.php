ffdfdec72bcf3a4b448e86b294461d4ec5ac93e3
rahul <rahul@divyaltech.com>
2024-01-16 11:38:03 +0530

    update code

---
+++ tyers_list.php
@@ -0,0 +1,252 @@
+<?php
+    include 'includes/headertag.php';
+    include 'includes/headertagadmin.php';
+   include 'includes/footertag.php';
+   
+   ?> 
+  <script> var APIBaseURL = "<?php echo $APIBaseURL; ?>";</script>
+  <script> var baseUrl = "<?php echo $baseUrl; ?>";</script>
+  <script src="<?php $baseUrl; ?>model/new_tyre_list.js"></script>
+  
+<body class="loaded"> 
+<div class="main-wrapper">
+    <div class="app" id="app">
+    <?php
+    include 'includes/left_nav.php';
+    include 'includes/header_admin.php';
+    ?>
+   <section style="padding: 0 15px 0 60px;">
+    <div class="">
+      <div class="">
+      <div class="card-body d-flex align-items-center justify-content-between page_title">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0">
+              
+              <li class="breadcrumb-item">
+                <span>Tyres Listings</span>
+              </li>
+            </ol>
+          </nav>
+          <button type="button" id="add_trac" class="btn add_btn btn-success float-right btn_all" data-bs-toggle="modal"  data-bs-target="#staticBackdrop">
+              <i class="fa fa-plus" aria-hidden="true"></i> Add New Tyre
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+              <div class="modal-content modal_box">
+                <div class="modal-header modal_head">
+                  <h5 class="modal-title text-white fw-bold" id="staticBackdropLabel">Add Tyre </h5>
+                  <button type="button" class="btn-close btn-success" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body bg-white">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-10">
+                              <h4 class="text-center" style="font-weight:600;">Fill your Details</h4>
+                              <form id="form_tyre_list">
+                                <div class="row justify-content-center">
+                                    <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3 ">
+                                      <div class="form-outline">
+                                        <label for="name" class="form-label text-dark">Brand</label>
+                                        <select class="form-select form-control" aria-label=".form-select-lg example" id="brand" name="brand">
+                                    
+                                    </select>
+                                      </div>
+                                    </div>
+                                    <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3">
+                                      <div class="form-outline ">
+                                        <label for="name" class="form-label text-dark">Tyre Model</label>
+                                        <input type="text" class="form-control" placeholder="" id="tyre" name="tyre">
+                                      </div>
+                                    </div>
+                                    <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3">
+                                      <div class="form-outline ">
+                                        <label for="name" class="form-label text-dark">Tyre Position</label>
+                                        <input type="text" class="form-control" placeholder="" id="tyre_position" name="tyre_position">
+                                      </div>
+                                    </div>
+                                    <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3">
+                                      <div class="form-outline ">
+                                        <label for="name" class="form-label text-dark">Size of the tyre</label>
+                                        <input type="text" class="form-control" placeholder="" id="tyre_size" name="tyre_size">
+                                      </div>
+                                    </div>
+                                  <!--   <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3">
+                                      <div class="form-outline ">
+                                        <label for="name" class="form-label text-dark">Tyre Diameter</label>
+                                        <input type="text" class="form-control" placeholder="" id="tyre_diameter" name="tyre_diameter">
+                                      </div>
+                                    </div> -->
+                                    <div class="col-12 col-lg-6 col-md-6 col-sm-6 mt-3">
+                                      <div class="form-outline ">
+                                        <label for="name" class="form-label text-dark">Tyre Width</label>
+                                        <input type="text" class="form-control" placeholder="" id="tyre_width" name="tyre_width">
+                                      </div>
+                                    </div> 
+                                    <div class="col-12 col-sm-12 col-md-6 col-lg-6 mt-3">
+                                     <div class="form-outline ">
+                                      <label for="yr_state" class="form-label text-dark">Category</label>
+                                      <select class="form-select form-control" aria-label=".form-select-lg example"id="category" name="category">
+                                         
+                                      </select>
+                                    </div>
+                                  </div>
+                                    <div class="col-12 col-lg-12 col-md-12 col-sm-12">
+                                      <div class="upload__box">
+                                        <div class="upload__btn-box text-center">
+                                          <label >
+                                            <p class="upload__btn ">Upload images</p>
+                                            <input type="file" multiple="" data-max_length="20" class="upload__inputfile" id="_image" name="_image">
+                                          </label>
+                                          <!-- <p></p> -->
+                                        </div>
+                                        <div id="selectedImagesContainer1" class="upload__img-wrap row"></div>
+                                      </div>
+                                    </div>
+                                   
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn_all" data-bs-dismiss="modal">Close</button>
+                  <button type="button" id="submit_btn" class="btn btn-success  btn_all">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="">
+      <!-- Filter Card -->
+      <div class="filter-card">
+        <div class="card-body">
+        <form action="" id="myform" class="mb-0">
+          <div class="row">
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 mt-3">
+              <div class="form-outline">
+                <label class="form-label">Brand</label>
+                <select class="form-select form-control" aria-label="Default select example" id="brand1">
+                 
+                </select>
+              </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4 mt-3">
+              <div class="form-outline">
+                <label class="form-label">Tyre Position</label>
+                <select class="form-select form-control" aria-label="Default select example">
+                  <option selected disabled>Select Position</option>
+                  <option value="1">Front-Left</option>
+                  <option value="2">Front-right</option>
+                  <option value="2">Back-Left</option>
+                  <option value="2">Back-right</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4  mt-3">
+              <div class="text-center">
+                <button type="button" class="btn-success btn btn_all" id="Search">Search</button>
+                <button type="button" class="btn-success btn  btn_all" id="Reset">Reset</button>
+              </div>
+            </div>
+          </div>
+        </form>
+        </div>
+      </div>
+      <!-- Table Card -->
+      <div class=" mb-5 shadow bg-white mt-3 p-3">
+            <div class="table-responsive">
+              <table id="example" class="table table-striped  table-hover table-bordered dataTable no-footer" width="100%; margin-bottom: 15px;">
+                <thead class="">
+                  <tr>
+                <th class="d-none d-md-table-cell text-white">S.No.</th>
+                <th class="d-none d-md-table-cell text-white">Brand</th>
+                <th class="d-none d-md-table-cell text-white">Model</th>
+                <th class="d-none d-md-table-cell text-white">Tyre Position</th>
+                <th class="d-none d-md-table-cell text-white">Size</th>
+                <th class="d-none d-md-table-cell text-white">Action</th>
+              </tr>
+            </thead>
+          <tbody id="data-table">
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-md modal-dialog-centered">
+              <div class="modal-content modal_box">
+                <div class="modal-header modal_head">
+                  <h5 class="modal-title text-white fw-bold" id="staticBackdropLabel">Tyre Information</h5>
+                  <button type="button" class="btn-close btn-success" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body bg-white">
+                  
+                <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <h6 class="fw-bold">Brand Name: </h6>
+                    </div>
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <p id="brand_name2" ></p>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <h6 class="fw-bold">Tyre Model:</h6>
+                    </div>
+                    <div class="col-12  col-lg-6 col-sm-6 col-md-6">
+                     <div id="model2"></div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <h6 class="fw-bold">Tyre Position:</h6>
+                    </div>
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                     <div id="quantity"></div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <h6 class="fw-bold">Size of Tyre:</h6>
+                    </div>
+                    <div class="col-12  col-lg-6 col-sm-6 col-md-6">
+                     <div id="grade"></div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <h6 class="fw-bold">Category:</h6>
+                    </div>
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                     <div id="price"></div>
+                    </div>
+                  </div>
+                  
+                  <div class="row">
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                      <!-- <h6 class="fw-bold">Image:</h6> -->
+                    </div>
+                    <div class="col-12 col-lg-6 col-sm-6 col-md-6">
+                    <div id="selectedImagesContainer" class="upload__img-wrap row"></div>
+                    </div>
+                  </div>
+                    </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary btn_all" data-bs-dismiss="modal">Close</button>
+                  </div>
+              </div>
+            </div>
+          </div>
+    
+</div>
+</div>
+</body>
+
+<?php
+   include 'includes/footertag.php';
+   ?> 
