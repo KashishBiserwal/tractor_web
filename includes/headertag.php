7328a04df88f61fb1013a6b34ecf6c094596431a
rahul <rahul@divyaltech.com>
2024-02-24 18:37:16 +0530

    update code

--- includes/headertag.php
+++ includes/headertag.php
@@ -3,6 +3,8 @@
  $CustomerAPIBaseURL = 'http://tractor-api.divyaltech.com/api/customer/';
 //  $baseUrl = '/tractor-project/tractor-project/';
 $baseUrl = '/tractor_house/'; 
+
+
 //   $APIBaseURL = "http://192.168.1.41:8000/api/customer/";
 ?>
 <meta charset="UTF-8">
